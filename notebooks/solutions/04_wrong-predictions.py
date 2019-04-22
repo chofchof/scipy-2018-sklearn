6829c090bd0f07a0aff4491a571ee1860c13bb0f
Jin-Hwan CHO <jinhwan.cho@gmail.com>
2019-04-22 17:44:35 +0900

    Update notebook 4

--- notebooks/solutions/04_wrong-predictions.py
+++ notebooks/solutions/04_wrong-predictions.py
@@ -3,22 +3,13 @@ for i in incorrect_idx:
 
 # Plot two dimensions
 
-colors = ["darkblue", "darkgreen", "gray"]
-
-for n, color in enumerate(colors):
+_, ax = plt.subplots()
+for n in np.unique(test_y):
     idx = np.where(test_y == n)[0]
-    plt.scatter(test_X[idx, 1], test_X[idx, 2],
-                color=color, label="Class %s" % str(n))
+    ax.scatter(test_X[idx, 1], test_X[idx, 2], color=f"C{n}", label=f"Class {str(n)}")
 
 for i, marker in zip(incorrect_idx, ['x', 's', 'v']):
-    plt.scatter(test_X[i, 1], test_X[i, 2],
-                color="darkred",
-                marker=marker,
-                s=40,
-                label=i)
+    ax.scatter(test_X[i, 1], test_X[i, 2], color="darkred", marker=marker, s=40, label=i)
 
-plt.xlabel('sepal width [cm]')
-plt.ylabel('petal length [cm]')
-plt.legend(loc=1, scatterpoints=1)
-plt.title("Iris Classification results")
-plt.show()
+ax.set(xlabel='sepal width [cm]', ylabel='petal length [cm]', title="Iris Classification results")
+plt.legend(loc=1, scatterpoints=1);
